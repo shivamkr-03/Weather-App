c9d7718f41521d2279166ab15c14887a0ae8e1c8
shivamkr-03 <shivamkmr.0308@gmail.com>
2025-03-09 14:57:53 +0530

    docs folder added

--- docs/script.js
+++ docs/script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    const api_key = "ee3d14c85efd5170c546fdda53197ef7";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
